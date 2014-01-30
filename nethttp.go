b46f3fc6fe4c5a9e21b9f5c62ce24b4eb8322237
Stephen Nguyen <nguyenhstephen@gmail.com>
2014-01-30 01:12:42 -0800

    prints and creates a html file of the response body

--- nethttp.go
+++ nethttp.go
@@ -3,7 +3,9 @@ package main
 import (
 	"net/http"
   "fmt"
-  "io/ioutil"
+  // "io/ioutil"
+  "io"
+  "os"
 )
 
 func main() {
@@ -13,8 +15,9 @@ if err != nil {
 fmt.Println("ERROR")
 }
 defer resp.Body.Close()
-body, err := ioutil.ReadAll(resp.Body)
-
-fmt.Println(string(body))
+fmt.Println(resp)
+// body, err := ioutil.ReadAll(resp.Body)
+out, err := os.Create("filename.html")
+io.Copy(out, resp.Body)
 
 }
